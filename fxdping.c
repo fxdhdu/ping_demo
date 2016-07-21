cd09a5e7f9eadef14e8f3fc9844e97d91415796f
fxd <757866311@qq.com>
2016-07-21 19:54:46 +0800

    添加了一些注释

--- fxdping.c
+++ fxdping.c
@@ -12,13 +12,13 @@
 #include<arpa/inet.h>  
 #include<pthread.h>  
   
-struct sockaddr_in dst_addr;  //标识，套接字地址
+struct sockaddr_in dst_addr;  //目的端标识，套接字地址结构
 struct sockaddr_in recv_addr;  
-struct timeval tvrecv;  
-char icmp_pkt[1024] = {0};  
-char recv_pkt[1024] = {0};  
-int sockfd = 0, bytes = 56, nsend_pkt = 0, nrecv_pkt = 0;  
-pid_t pid;  
+struct timeval tvrecv;  //收到数据报的时间
+char icmp_pkt[1024] = {0};  //请求报文
+char recv_pkt[1024] = {0};  //应答报文
+int sockfd = 0, bytes = 56, nsend_pkt = 0, nrecv_pkt = 0;  //发送和接收报文的个数
+pid_t pid; //进程ID 
   
 void statistics();  
 int in_chksum(unsigned short *buf, int size);  
@@ -30,14 +30,14 @@ void tv_sub(struct timeval *out,struct timeval *in);
   
 int main(int argc, char **argv)  
 {  
-    int size = 50 * 1024;  
+    int size = 50 * 1024;  //接收缓冲区的字节长度
     int errno = -1;  
-    int ttl = 64;  
+    int ttl = 64;  //数据报生存周期
     void *tret;  
     pthread_t send_id,recv_id;  //线程ID
-    struct in_addr ipv4_addr;  
+    struct in_addr ipv4_addr;  //IPv4地址，sockaddr_in中的一部分
     struct hostent *ipv4_host;  
-    struct protoent *protocol = NULL;  
+    struct protoent *protocol = NULL;  //协议信息
 // struct protoent {
 //        char  *p_name;       /* official protocol name */
 //        char **p_aliases;    /* alias list */
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
         printf("usage: ./ping <host>\n");  
         return -1;  
     }  
-    if ((protocol = getprotobyname("icmp")) == NULL) //返回对于给定协议名的相关协议信息 
+    if ((protocol = getprotobyname("icmp")) == NULL) //返回对于给定协议名的相关协议信息;检查是否有协议相关信息 
     {  
         printf("unkown protocol\n");  
         return -1;  
@@ -62,13 +62,18 @@ int main(int argc, char **argv)
         return -1;  
     }  
     //setsockopt为套接字选项接口，用于控制套接字行为。
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));  //选项是通用的套接字层次选项，选项为：接受缓冲区的字节长度
-    setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));  //IP是控制这个选项的协议
-    setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));  //IP
-  
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));  //选项是通用的套接字层次选项，选项为：接收缓冲区的字节长度
+    setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));  //IP是控制这个选项的协议，设置组播
+    setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));  //IP，设置主机发送数据报的生存时间
+/*type = struct sockaddr_in {
+    sa_family_t sin_family;
+    in_port_t sin_port;
+    struct in_addr sin_addr;
+    unsigned char sin_zero[8];
+}*/
     memset(&dst_addr, 0, sizeof(dst_addr));  
-    dst_addr.sin_family = AF_INET;  
-    errno = inet_aton(argv[1], &ipv4_addr);  
+    dst_addr.sin_family = AF_INET; 
+    errno = inet_aton(argv[1], &ipv4_addr);  //将Internet主机地址，从点分十进制转化为二进制格式（网络字节序）
     if (errno == 0)  //地址无效
     {  
         ipv4_host = gethostbyname(argv[1]);  //通过主机名得到地址
@@ -106,7 +111,7 @@ int main(int argc, char **argv)
     return 0;  
 }  
   
-void statistics()  
+void statistics() //统计发送和接收数据报的书目，计算丢包率。（若一次发送数据报没有得到回应，则代表丢包） 
 {  
     printf("\n--- %s ping statistics ---\n", inet_ntoa(dst_addr.sin_addr));  
     printf("%d packets transmitted, %d received, %.3f%c packet loss\n",  
@@ -163,10 +168,10 @@ int pack(int send_pkt)  //构建ICMP报文
         u_int8_t id_data[1];
     } icmp_dun;
 }*/ 
-    pkt->icmp_type = ICMP_ECHO;  
-    pkt->icmp_cksum = 0;  
-    pkt->icmp_seq = htons(nsend_pkt);  
-    pkt->icmp_id = pid;  
+    pkt->icmp_type = ICMP_ECHO;  //类型
+    pkt->icmp_cksum = 0;  //校验和
+    pkt->icmp_seq = htons(nsend_pkt); //序号  
+    pkt->icmp_id = pid;  //标识符字段：置成发送进程的ID号
     time = (struct timeval *)pkt->icmp_data;  
     gettimeofday(time, NULL);  
     pkt->icmp_cksum = in_chksum((unsigned short *)pkt, bytes + 8);  
@@ -212,7 +217,7 @@ int unpack(char *recv_pkt, int size)
     int iphdrlen;  
     struct icmp *icmp;  
     struct timeval *tvsend;  
-    double rtt;  
+    double rtt;  //往返时间
   
     ip = (struct iphdr *)recv_pkt;  
     iphdrlen = ip->ihl<<2;  
@@ -242,8 +247,8 @@ int unpack(char *recv_pkt, int size)
   
 void *recv_ping()  
 {  
-    fd_set rd_set;  
-    struct timeval time;  
+    fd_set rd_set;  //一个很大的字节数组，每一个可能的描述符保持1位
+    struct timeval time;  //I/O多路转接需要等待的时间长度
     time.tv_sec = 5;  
     time.tv_usec = 0;  
     int ret = 0, nread = 0,recv_len = 0;  
@@ -251,15 +256,15 @@ void *recv_ping()
     recv_len = sizeof(recv_addr);  
     while(1)  
     {  
-        FD_ZERO(&rd_set);  
-        FD_SET(sockfd, &rd_set);  
+        FD_ZERO(&rd_set);  //所有位设置为0
+        FD_SET(sockfd, &rd_set);  //开启描述符中的一位，sockfd
         ret = select(sockfd + 1, &rd_set, NULL, NULL, &time);  
 	//执行I/O多路转接。time:愿意等待的时间长度。rd_set指向读描述符集的指针。第一个参数：最大文件描述符编号值加1
-        if (ret <= 0)  
+        if (ret <= 0)  //等待超时或出错
         {  
             continue;  
         }  
-        else if (FD_ISSET(sockfd, &rd_set))  //测试描述符集中的一个指定位是否已经打开
+        else if (FD_ISSET(sockfd, &rd_set))  //测试描述符集中的sockfd位是否仍处于打开状态
         {  
             nread = recvfrom(sockfd, recv_pkt, sizeof(recv_pkt), 0, (struct sockaddr *)&recv_addr,(socklen_t *) &recv_len);  
             if (nread < 0)  
